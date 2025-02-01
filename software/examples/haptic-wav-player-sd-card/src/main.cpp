b33ecd064e582e58af15aebc161aebe96409e765
Mads Kjeldgaard <mail@madskjeldgaard.dk>
2025-02-01 18:45:01 +0100

    finish wav player example

--- software/examples/haptic-wav-player-sd-card/src/main.cpp
+++ software/examples/haptic-wav-player-sd-card/src/main.cpp
@@ -1,163 +1,137 @@
-// Simple WAV file player example
-//
-// Three types of output may be used, by configuring the code below.
-//
-//   1: Digital I2S - Normally used with the audio shield:
-//         http://www.pjrc.com/store/teensy3_audio.html
-//
-//   2: Digital S/PDIF - Connect pin 22 to a S/PDIF transmitter
-//         https://www.oshpark.com/shared_projects/KcDBKHta
-//
-//   3: Analog DAC - Connect the DAC pin to an amplified speaker
-//         http://www.pjrc.com/teensy/gui/?info=AudioOutputAnalog
-//
-// To configure the output type, first uncomment one of the three
-// output objects.  If not using the audio shield, comment out
-// the sgtl5000_1 lines in setup(), so it does not wait forever
-// trying to configure the SGTL5000 codec chip.
-//
-// The SD card may connect to different pins, depending on the
-// hardware you are using.  Uncomment or configure the SD card
-// pins to match your hardware.
-//
-// Data files to put on your SD card can be downloaded here:
-//   http://www.pjrc.com/teensy/td_libs_AudioDataFiles.html
-//
-// This example code is in the public domain.
+/*
+This sketch reads the SD card and plays all WAV files on it.
 
+If it's patched to the haptics driver (as it is per default), all audio should
+cause the driver to activate.
+*/
+
+#include "Adafruit_DRV2605.h"
+#include <Adafruit_NeoPixel.h>
 #include <Audio.h>
 #include <SD.h>
 #include <SPI.h>
 #include <SerialFlash.h>
 #include <Wire.h>
-#include <bufferedaudio/play_wav_buffered.h>
 
-AudioPlayWAVstereo playWav1;
-// Use one of these 3 output types: Digital I2S, Digital S/PDIF, or Analog DAC
-AudioOutputI2S audioOutput;
-// AudioOutputSPDIF       audioOutput;
-// AudioOutputAnalog      audioOutput;
-// On Teensy LC, use this for the Teensy Audio Shield:
-// AudioOutputI2Sslave    audioOutput;
+constexpr auto audioDirectory = "/audiofiles";
 
-AudioConnection patchCord1(playWav1, 0, audioOutput, 0);
-AudioConnection patchCord2(playWav1, 1, audioOutput, 1);
-AudioControlSGTL5000 sgtl5000_1;
+// NeoPixel object
+constexpr auto LED_PIN = 5;
+constexpr auto NUM_LEDS = 1;
+Adafruit_NeoPixel strip(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-// Use these with the Teensy Audio Shield
-#define SDCARD_CS_PIN 10
-#define SDCARD_MOSI_PIN 7 // Teensy 4 ignores this, uses pin 11
-#define SDCARD_SCK_PIN 14 // Teensy 4 ignores this, uses pin 13
+// Audio patch
+AudioPlaySdWav playWav; // Add AudioPlaySdWav object
+AudioOutputI2S i2s1;    // xy=365,94
+AudioConnection patchCord1(playWav, 0, i2s1, 0);
+AudioConnection patchCord2(playWav, 1, i2s1, 1);
 
-// Use these with the Teensy 3.5 & 3.6 & 4.1 SD card
-// #define SDCARD_CS_PIN    BUILTIN_SDCARD
-// #define SDCARD_MOSI_PIN  11  // not actually used
-// #define SDCARD_SCK_PIN   13  // not actually used
+constexpr auto cs = 10;
+constexpr auto mosi = 11;
+constexpr auto sck = 13;
+constexpr auto miso = 12;
 
-// Use these for the SD+Wiz820 or other adaptors
-// #define SDCARD_CS_PIN    4
-// #define SDCARD_MOSI_PIN  11
-// #define SDCARD_SCK_PIN   13
+// Haptics driver
+Adafruit_DRV2605 drv;
 
-void postPerformanceOfTeensy() {
-  // print a summary of the current & maximum usage
-  Serial.print("all=");
-  Serial.print(AudioProcessorUsage());
-  Serial.print(",");
-  Serial.print(AudioProcessorUsageMax());
-  Serial.print("    ");
-  Serial.print("Memory: ");
-  Serial.print(AudioMemoryUsage());
-  Serial.print(",");
-  Serial.print(AudioMemoryUsageMax());
-  Serial.println();
+bool setupHapticDriver() {
+  auto result = drv.begin();
+
+  drv.setMode(DRV2605_MODE_AUDIOVIBE);
+
+  // ac coupled input, puts in 0.9V bias
+  drv.writeRegister8(DRV2605_REG_CONTROL1, 0x20);
+
+  // analog input
+  drv.writeRegister8(DRV2605_REG_CONTROL3, 0xA3);
+
+  return result;
 }
 
-constexpr auto bufferSize = 4096 * 4;
+void setLEDGreen() {
+  const auto color = strip.Color(0, 255, 0);
+  strip.fill(color, 0, NUM_LEDS);
+  strip.show();
+}
+
+void setLEDRed() {
+  const auto color = strip.Color(255, 0, 0);
+  strip.fill(color, 0, NUM_LEDS);
+  strip.show();
+}
+
+void setLEDOrange() {
+  const auto color = strip.Color(255, 165, 0);
+  strip.fill(color, 0, NUM_LEDS);
+  strip.show();
+}
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  strip.begin();
+  setLEDRed();
 
-  while (!Serial && millis() < 4000) {
-  }; // wait up to 4 seconds for the Serial Monitor
+  Serial.println("Setting up haptic driver...");
+  auto hapticOK = setupHapticDriver();
+  if (!hapticOK) {
+    Serial.println("Haptic driver setup failed");
+    while (1) {
+      delay(10);
+    };
+  }
 
-  // Audio connections require memory to work.  For more
-  // detailed information, see the MemoryAndCpuUsage example
-  AudioMemory(8);
+  Serial.println("Allocating audio memory...");
+  AudioMemory(10);
 
-  // Buffered playback object requires a buffer to be allocated. Pick one of
-  // these:
-  playWav1.createBuffer(bufferSize,
-                        AudioBuffer::inHeap); // everything has heap memory
-  // playWav1.createBuffer(4096,AudioBuffer::inExt); // works on a Teensy 4.1
-  // with PSRAM fitted
-  //*
-  // {
-  //   static uint8_t audioBuffer[bufferSize];
-  //   playWav1.createBuffer(audioBuffer, bufferSize); // application-managed
-  //   memory
-  // }
-  //*/
+  SPI.setMOSI(mosi);
+  SPI.setSCK(sck);
+  SPI.setMISO(miso);
 
-  // Comment these out if not using the audio adaptor board.
-  // This may wait forever if the SDA & SCL pins lack
-  // pullup resistors
-  // sgtl5000_1.setAddress(HIGH);
-  sgtl5000_1.enable();
-  sgtl5000_1.volume(0.5);
-
-  SPI.setMOSI(SDCARD_MOSI_PIN);
-  SPI.setSCK(SDCARD_SCK_PIN);
-  while (!(SD.begin(SDCARD_CS_PIN))) {
-    // stop here, but print a message repetitively
-    Serial.println("Unable to access the SD card");
-    delay(500);
+  Serial.println("Initializing SD card...");
+  if (!SD.begin(cs)) {
+    Serial.println("SD card initialization failed!");
+    while (1) {
+      delay(10);
+    }
   }
 }
 
-void playFile(const char *filename) {
-  Serial.print("Playing file: ");
-  Serial.println(filename);
-
-  // Start playing the file.  This sketch continues to
-  // run while the file plays.
-  playWav1.play(filename);
-
-  // A brief delay for the library read WAV info
-  // is NOT needed for the buffered playback objects
-  // delay(25);
-
-  // Simply wait for the file to finish playing.
-  while (playWav1.isPlaying()) {
-    // yield();    // loop() isn't exiting, so allow EventResponder to re-load
-    // the buffers
-
-    postPerformanceOfTeensy();
-    delay(10); // this works as well - use either
-
-    // uncomment these lines if your audio shield
-    // has the optional volume pot soldered
-    // float vol = analogRead(15);
-    // vol = vol / 1024;
-    // sgtl5000_1.volume(vol);
+void playNextWavFile(File dir) {
+  File entry = dir.openNextFile();
+  if (!entry) {
+    // no more files
+    return;
+  }
+  if (!entry.isDirectory()) {
+    // we only care about .WAV files
+    String filename = entry.name();
+    const auto weird_file =
+        filename.startsWith(".") || filename.startsWith("_");
+    if (filename.endsWith(".WAV") ||
+        ((filename.endsWith(".wav") && !weird_file))) {
+      {
+        Serial.print("Playing file: ");
+        Serial.println(filename);
+        playWav.play(filename.c_str());
+        delay(5); // wait for library to parse WAV info
+        while (playWav.isPlaying()) {
+          setLEDOrange();
+          delay(100); // Add a small delay to avoid flooding the serial output
+        }
+      }
+    } else {
+      Serial.print("Skipping file: ");
+      Serial.println(filename);
+    }
+    entry.close();
   }
 }
-
 
 void loop() {
-  playFile("audio/BEACH.wav"); // filenames are always uppercase 8.3 format
-  Serial.println(millis());
-  postPerformanceOfTeensy();
-  delay(500);
-  Serial.println("done");
-  playFile("audio/BEACH.wav"); // filenames are always uppercase 8.3 format
-  Serial.println(millis());
-  postPerformanceOfTeensy();
-  delay(500);
-  Serial.println("done");
-  playFile("audio/BEACH.wav"); // filenames are always uppercase 8.3 format
-  Serial.println(millis());
-  postPerformanceOfTeensy();
-  delay(500);
-  Serial.println("done");
-  postPerformanceOfTeensy();
+  setLEDOrange();
+  if (!playWav.isPlaying()) {
+    File root = SD.open(audioDirectory);
+    playNextWavFile(root);
+    root.close();
+  }
 }
